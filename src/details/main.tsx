ef38e7faf3ee74b148cf7e6652669e2dde67c5da
jsimpauco <jms.simpauco@gmail.com>
2026-08-08 16:53:47 -0700

    testing multiple pages

--- src/details/main.tsx
+++ src/details/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../styles/index.css'
-import Home from './Home.tsx'
+import Details from './Details.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
+    <StrictMode>
+        <Details />
+    </StrictMode>,
 )
